72519da5e7aed24f261763700b781e0ad2babce5
seungjin <xeungjin@nate.com>
2026-07-01 13:39:53 +0900

    Initial commit

---
+++ mall/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <h1 className="text-4xl font-bold text-blue-600">
+            Hello World!
+        </h1>
+    );
+}
+
+export default App;
