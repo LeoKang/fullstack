ac34bdb587a8484276424ae112f502acf017d994
seungjin <xeungjin@nate.com>
2026-07-01 16:55:47 +0900

    62page

--- mall/src/router/root.js
+++ mall/src/router/root.js
@@ -6,19 +6,26 @@ const Loading = <div>Loading...</div>
 const Main = lazy(() => import("../pages/MainPage"))
 const About = lazy(() => import("../pages/AboutPage"))
 const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+const TodoList = lazy(()=>import("../pages/todo/ListPage"))
 
 const root = createBrowserRouter([
     {
         path: "",
-        element: <Suspense fallback={Loading}><Main /></Suspense>
+        element: <Suspense fallback={Loading}><Main/></Suspense>
     },
     {
         path: "about",
-        element: <Suspense fallback={Loading}><About /></Suspense>
+        element: <Suspense fallback={Loading}><About/></Suspense>
     },
     {
         path: "todo",
-        element: <Suspense fallback={Loading}><TodoIndex /></Suspense>
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: [
+            {
+                path: "list",
+                element: <Suspense fallback={Loading}><TodoList/></Suspense>
+            }
+        ]
     }
 ])
 
