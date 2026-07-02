9273aa4fd0b7d078b5f2c726cd5d21fcc573a2a5
seungjin <xeungjin@nate.com>
2026-07-02 15:41:16 +0900

    96 page - testDelete()

--- mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
+++ mallapi/src/test/java/org/zerock/mallapi/repository/TodoRepositoryTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.mallapi.domain.Todo;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @SpringBootTest
 @Log4j2
@@ -34,4 +35,32 @@ public class TodoRepositoryTests {
         }
     }
 
+    @Test
+    public void testRead() {
+        Long tno = 233L;
+
+        java.util.Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElseThrow();
+        log.info(todo);
+    }
+
+    @Test
+    public void testModify() {
+        Long tno = 33L;
+
+        Optional<Todo> result = todoRepository.findById(tno);
+        Todo todo = result.orElseThrow();
+        todo.changeTitle("Modified " + tno + "....");
+        todo.changeComplete(true);
+        todo.changeDueDate(LocalDate.of(2026, 7, 2));
+
+        todoRepository.save(todo);
+    }
+
+    @Test
+    public void testDelete() {
+        Long tno = 1L;
+        todoRepository.deleteById(tno);
+    }
 }
+
