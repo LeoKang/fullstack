8b2f7a479d549be0040246e6cf85c496ff398278
seungjin <xeungjin@nate.com>
2026-07-02 11:56:48 +0900

    76 page

--- mall/src/pages/todo/ReadPage.js
+++ mall/src/pages/todo/ReadPage.js
@@ -1,4 +1,4 @@
-import {useNavigate, useParams} from "react-router-dom";
+import {createSearchParams, useNavigate, useParams, useSearchParams} from "react-router-dom";
 import {useCallback} from "react";
 
 const ReadPage = () => {
@@ -7,15 +7,23 @@ const ReadPage = () => {
 
     const navigate = useNavigate()
 
+    const [queryParams] = useSearchParams()
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+    const queryStr = createSearchParams({page,size}).toString()
+
     const moveToModify = useCallback((tno) => {
-        navigate({pathname: `/todo/modify/${tno}`})
-    }, [tno])
+        navigate({
+            pathname: `/todo/modify/${tno}`,
+            search: queryStr
+        })
+    }, [tno, page, size])
 
     return (
         <div className="text-3xl font-extrabold">
             Todo Read Page Component {tno}
             <div>
-                <button onClick={() => moveToModify(33)}>Test Modify</button>
+                <button onClick={() => moveToModify(tno)}>Test Modify</button>
             </div>
         </div>
     )
