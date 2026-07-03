8a3f676f487c4c93ab6a2437c67b10f5adfb7029
seungjin <xeungjin@nate.com>
2026-07-03 11:18:23 +0900

    104 page

--- mallapi/src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
+++ mallapi/src/main/java/org/zerock/mallapi/service/TodoServiceImpl.java
@@ -1,17 +1,31 @@
 package org.zerock.mallapi.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.zerock.mallapi.domain.Todo;
 import org.zerock.mallapi.dto.TodoDTO;
+import org.zerock.mallapi.repository.TodoRepository;
 
 @Service
 @Transactional
 @Log4j2
+@RequiredArgsConstructor
 public class TodoServiceImpl implements TodoService {
+
+    private final ModelMapper modelMapper;
+
+    private final TodoRepository todoRepository;
+
     @Override
     public Long register(TodoDTO todoDTO) {
         log.info("TodoServiceImple - register() ------------");
-        return 0L;
+
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
+        Todo savedTodo = todoRepository.save(todo);
+
+        return savedTodo.getTno();
     }
 }
