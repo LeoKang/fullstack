d26efe5e5d993c49707fbe23b4d695d066376e0b
seungjin <xeungjin@nate.com>
2026-07-06 12:42:28 +0900

    .

--- mall/src/hooks/useCustomMove.js
+++ mall/src/hooks/useCustomMove.js
@@ -1,34 +1,37 @@
 import {createSearchParams, useNavigate, useSearchParams} from "react-router-dom";
 
 const getNum = (param, defaultValue) => {
-    if (!param) {
-        return defaultValue;
-    }
-    return parseInt(param);
-}
+  if (!param) {
+    return defaultValue;
+  }
+
+  return parseInt(param);
+};
 
 const useCustomMove = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [queryParams] = useSearchParams();
-    const page = getNum(queryParams.get('page'), 1)
-    const size = getNum(queryParams.get('size'), 10)
+  const [queryParams] = useSearchParams();
 
-    const queryDefault = createSearchParams({page, size}).toString();
+  const page = getNum(queryParams.get("page"), 1);
+  const size = getNum(queryParams.get("size"), 10);
 
-    const moveToList = (pageParam) => {
-        let queryStr = ""
+  const queryDefault = createSearchParams({ page, size }).toString();
 
-        if (pageParam) {
-            const pageNum = getNum(pageParam.page, 1)
-            const sizeNum = getNum(pageParam.size, 10)
-
-            queryStr = createSearchParams({page: pageNum, size: sizeNum}).toString()
-        } else {
-            queryStr = queryDefault
-        }
-        navigate({pathname: `../list`, search:queryStr})
+  const moveToList = (pageParam) => {
+    let queryStr = "";
+    if (pageParam) {
+      const pageNum = getNum(pageParam.page, 1);
+      const sizeNum = getNum(pageParam.size, 10);
+      queryStr = createSearchParams({
+        page: pageNum,
+        size: sizeNum,
+      }).toString();
+    } else {
+      queryStr = queryDefault;
     }
+    navigate({ pathname: `../list`, search: queryStr });
+  };
 
     const moveToModify = (num) => {
         console.log(queryDefault)
@@ -41,4 +44,4 @@ const useCustomMove = () => {
 
     return {moveToList, moveToModify, page, size}
 }
-export default useCustomMove
+export default useCustomMove;
